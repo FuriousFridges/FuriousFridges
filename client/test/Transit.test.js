9d5be8b686839af049a02bbd41a2fdb5cb9e0b02
Kara Marnell <kmarnell@gmail.com>
2017-06-16 11:53:27 -0700

    attempt front end tests

--- client/test/Transit.test.js
+++ client/test/Transit.test.js
@@ -1,11 +1,44 @@
 import React from 'react';
-import { shallow, mount, render } from 'enzyme';
 import Transit from '../../client/src/components/Transit.jsx';
+import renderer from 'react-test-renderer';
+import { shallow, mount, render } from 'enzyme';
 
 
-describe('A suite to test the Transit component', () => {
- 
-  test('contains spec with an expectation', function() {
-    expect(true).toBeTruthy();
-  });
+it('renders correctly', () => {
+  const tree = renderer.create(
+    <Transit />
+  ).toJSON();
+  expect(tree).toMatchSnapshot();
 });
+
+// describe('A suite to test the Transit component', () => {
+
+//   it('Expect Transit.jsx to render without throwing an error', () => {
+//      const wrapper = shallow(<Transit />);
+//       expect(wrapper).toMatchSnapshot();
+//    });
+// });
+
+
+
+// describe('A suite to test the Transit component', () => {
+ 
+  // test('contains spec with an expectation', function() {
+  //   expect(true).toBeTruthy();
+  // });
+
+//   it('should render <Transit />', () => {
+//     const wrapper = shallow(<Transit />);
+//     expect(wrapper).toMatchSnapshot();
+//   });
+
+// });
+
+
+//   test('Transit component should render as expected', () => {
+//   const component = shallow(<Transit />)
+//   const tree = toJson(component)
+
+//   expect(tree).toMatchSnapshot()
+// })
+
